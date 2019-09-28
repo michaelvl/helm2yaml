1895f6b1a6fda04b419318071c6948542fe1382e
MichaelVL <mvl.gh@network42.dk>
2019-09-28 16:35:58 +0200

    Update container image repo

--- helmsman.sh
+++ helmsman.sh
@@ -5,7 +5,7 @@ set -xe
 mkdir -p ./rendered
 
 # Map current folder as /src and assume all files passed with -f are relative to this
-docker run --rm -v $(pwd):/src:ro -v $(pwd)/rendered:/rendered:rw helm-up --create-namespace --render-to /rendered/out.yaml helmsman $@
+docker run --rm -v $(pwd):/src:ro -v $(pwd)/rendered:/rendered:rw michaelvl/helm-up --create-namespace --render-to /rendered/out.yaml helmsman $@
 
 # Namespace is missing
 cat rendered/out.yaml | envsubst | kubectl apply -f -
