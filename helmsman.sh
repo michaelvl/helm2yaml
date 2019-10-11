8be08776efa47c162d646f7d23aec62961f52c8a
MichaelVL <mvl.gh@network42.dk>
2019-10-11 18:05:44 +0200

    Add comment

--- helmsman.sh
+++ helmsman.sh
@@ -7,4 +7,5 @@ mkdir -p ./rendered
 # Map current folder as /src and assume all files passed with -f are relative to this
 docker run --rm -v $(pwd):/src:ro -v $(pwd)/rendered:/rendered:rw michaelvl/helm-up --create-namespace --render-to /rendered/out.yaml helmsman $@
 
+# Namespace is missing - 'helm template' does not add namespace
 cat rendered/out.yaml | envsubst | kubectl apply -f -
