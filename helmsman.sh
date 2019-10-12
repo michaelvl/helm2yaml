9a32d7237b61441a6c1f56f4737ac81a28378fec
MichaelVL <mvl.gh@network42.dk>
2019-10-12 14:09:55 +0200

    Add api versions

--- helmsman.sh
+++ helmsman.sh
@@ -5,7 +5,7 @@ set -xe
 mkdir -p ./rendered
 
 # Map current folder as /src and assume all files passed with -f are relative to this
-docker run --rm -v $(pwd):/src:ro -v $(pwd)/rendered:/rendered:rw michaelvl/helm2yaml --create-namespace --render-to /rendered/out.yaml helmsman $@
+docker run --rm -v $(pwd):/src:ro -v $(pwd)/rendered:/rendered:rw michaelvl/helm2yaml --create-namespace --api-versions apiregistration.k8s.io/v1beta1 --render-to /rendered/out.yaml helmsman $@
 
 # Namespace is missing - 'helm template' does not add namespace
 cat rendered/out.yaml | envsubst | kubectl apply -f -
