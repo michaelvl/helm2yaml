200e816180f47bcc3745a18720989376862f1734
MichaelVL <mvl.gh@network42.dk>
2019-11-02 17:52:17 +0100

    Sonobuoy container support for retrieving results

--- tools/sonobuoy.sh
+++ tools/sonobuoy.sh
@@ -6,5 +6,7 @@ set -e
 SONOBUOY_IMAGE=sonobuoy/sonobuoy:v0.16.2
 # Pass-in both the user .kube folder and the current value of KUBECONFIG
 KUBECFG="-v ${HOME}/.kube:${HOME}/.kube"
-SONOBUOY_CMD="docker run -ti --user $(id -u) --rm -e KUBECONFIG $KUBECFG:ro --entrypoint /sonobuoy ${SONOBUOY_IMAGE}"
+# 'sonobuoy retrieve' need to write data, hence the '-w' and this bind mount
+RESULT_MOUNT="-v $(pwd):/results"
+SONOBUOY_CMD="docker run -ti --user $(id -u) --rm -e KUBECONFIG $KUBECFG:ro $RESULT_MOUNT:rw -w /results --entrypoint /sonobuoy $SONOBUOY_IMAGE"
 ${SONOBUOY_CMD} "$@"
