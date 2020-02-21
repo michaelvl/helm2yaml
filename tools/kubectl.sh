67be58bf1ce28260038f2daa7bd5b03eda829952
MichaelVL <mvl.gh@network42.dk>
2020-02-21 16:58:50 +0100

    Allow local routing of API access by using host network

--- tools/kubectl.sh
+++ tools/kubectl.sh
@@ -7,5 +7,5 @@ KUBECTL_IMAGE=bitnami/kubectl:1.17.3
 # Pass-in both the user .kube folder and the current value of KUBECONFIG
 KUBECFG="-v ${HOME}/.kube:${HOME}/.kube"
 MANIFESTS="-v $(pwd):/work"
-KUBECTL_CMD="docker run -ti --user $(id -u):$(id -g) --rm -e KUBECONFIG $KUBECFG:ro -w /work $MANIFESTS:ro ${KUBECTL_IMAGE}"
+KUBECTL_CMD="docker run -ti --user $(id -u):$(id -g) --net host --rm -e KUBECONFIG $KUBECFG:ro -w /work $MANIFESTS:ro ${KUBECTL_IMAGE}"
 ${KUBECTL_CMD} "$@"
