1bf398e872a0fec6241ec0df429606832b2d5b97
MichaelVL <mvl.gh@network42.dk>
2020-03-08 14:35:34 +0100

    Add user and group settings to terraform

--- tools/terraform.sh
+++ tools/terraform.sh
@@ -4,5 +4,5 @@ set -x
 set -e
 
 TERRAFORM_IMAGE=hashicorp/terraform:0.12.3
-TERRAFORM_CMD="docker run -ti --rm -w /app -v `pwd`:/app ${TERRAFORM_IMAGE}"
+TERRAFORM_CMD="docker run -ti --user $(id -u):$(id -g) --rm -w /app -v `pwd`:/app ${TERRAFORM_IMAGE}"
 ${TERRAFORM_CMD} "$@"
