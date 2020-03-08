1a374316fa4418f5e8f4e75d39dadcc95130ea27
MichaelVL <mvl.gh@network42.dk>
2020-03-08 14:35:09 +0100

    Update image, remote tty, remove debug logs

--- tools/kustomize.sh
+++ tools/kustomize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -x
+#set -x
 set -e
 
-IMAGE=michaelvl/kustomize:20200131-1549-3e89bbf-v3.5.4
+IMAGE=michaelvl/kustomize:20200308-1332-de963de-v3.5.4
 MANIFESTS="-v $(pwd):/work"
-docker run -ti --user $(id -u):$(id -g) --rm -w /work $MANIFESTS:ro ${IMAGE} "$@"
+docker run -i --user $(id -u):$(id -g) --rm -w /work $MANIFESTS:ro ${IMAGE} "$@"
